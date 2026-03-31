b619a9b55c36ae7234e6df30cd655dde56e176fa
miguelrcha <miguellrochaxavier@gmail.com>
2026-03-31 09:47:39 -0300

    add activies 1 && 2 + list

--- Aula5  - Estrutura condicional e Estrutura de seleção/Exercicios/aula05-exercicio1.c
+++ Aula5  - Estrutura condicional e Estrutura de seleção/Exercicios/aula05-exercicio1.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-#include <math.h> // lib para utilizar o campo floor/ feito para identificar se o num e quebrado ou nao
 
-int main() {
-    double x;
+int main(void) {
+    float x;
 
     printf("Digite um numero: ");
-    scanf("%lf", &x);
+    scanf("%f", &x);
 
-    if (floor(x) == x) {
+    if (x == (int)x) {
         printf(" %.2f e inteiro", x);
     } else {
         printf("%.2f e quebrado", x);
--- Aula5  - Estrutura condicional e Estrutura de seleção/Exercicios/aula5-exercicio2.c
+++ Aula5  - Estrutura condicional e Estrutura de seleção/Exercicios/aula5-exercicio2.c
@@ -2,8 +2,31 @@
 
 int main (void) {
 
-    int x,y,z;
+    int x,y,z, temp;
     printf("Digite os 3 numero: ");
     scanf("%d %d %d", &x, &y, &z);
+    fflush(stdin);
 
+    if (x > y) {
+        temp = x;
+        x = y;
+        y = temp;
+    }
+
+    if (x > z) {
+        temp = x;
+        x = z;
+        z = temp;
+    }
+
+    if (y > z) {
+        temp = y;
+        y = z;
+        z = temp;
+    }
+
+    printf("Ordem crescente: %d %d %d\n", x, y, z);
+
+
+    return 0;
 }
