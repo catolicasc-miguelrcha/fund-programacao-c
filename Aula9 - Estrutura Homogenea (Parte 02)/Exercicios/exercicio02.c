e13943ebcce94c74a43be00e31757a1daf12ce7c
miguelrcha <miguellrochaxavier@gmail.com>
2026-05-04 19:41:30 -0300

    fix: int count in exercice 02

--- Aula9 - Estrutura Homogenea (Parte 02)/Exercicios/exercicio02.c
+++ Aula9 - Estrutura Homogenea (Parte 02)/Exercicios/exercicio02.c
@@ -9,6 +9,7 @@ int main (void) {
 
     int i, j;
     int matriz[4][4];
+    int count = 0;
 
     for (i = 0; i < 4; i++) {
         for (j = 0; j < 4; j++) {
@@ -21,11 +22,14 @@ int main (void) {
         for (j = 0; j < 4; j++) {
             if (matriz[i][j] >= 10) {
                 printf ("%d ", matriz[i][j]);
+                count++;
             }
         }
 
         printf("\n");
     }
+
+    printf ("Quantidade encontrada de numeros maiores ou iguais a 10 %d", count);
 
     return 0;
 }
