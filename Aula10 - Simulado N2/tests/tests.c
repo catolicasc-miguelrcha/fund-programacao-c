7f82ea204e246114d09a40277ed5c4da377e4cd3
miguelrcha <miguellrochaxavier@gmail.com>
2026-05-11 21:37:43 -0300

    test: simulado N2 atv10

--- Aula10 - Simulado N2/tests/tests.c
+++ Aula10 - Simulado N2/tests/tests.c
@@ -2,18 +2,36 @@
 // Created by miguel.xavier on 11/05/2026.
 //
 
-#include <stdio.h> 
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
-    int matriz[3][3];
-    int i, j, somaDP, somaDS = 0;
+    int m[3][3];
+    int d, s = 0;
 
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
-            printf("Enter a number: ");
-            scanf("%i", &matriz[i][j]);
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("Digite [%i,%i]: ", i, j);
+            scanf("%i", &m[i][j]);
+            fflush(stdin);
         }
     }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (i == j) {
+                d += m[i][j];
+            }
+
+            if (i + j == 2) {
+                s += m[i][j];
+            }
+        }
+        printf("\n");
+    }
+
+    printf("%i Diagonal pr\n", d);
+    printf("%i Diagonal sec\n", s);
 
     return 0;
 }
